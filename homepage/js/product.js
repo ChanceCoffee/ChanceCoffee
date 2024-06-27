0abe8e3593d3d35fd60ed7ccee5c0579f4141a93
jiheechai <jjihee97@naver.com>
2024-06-27 14:07:55 +0900

    product.js 정리

--- homepage/js/product.js
+++ homepage/js/product.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const productItems = document.querySelectorAll('#content .product_list li');
     let touchStartTime = 0;
     let isScrolling = false;
-    let activeItem = null; // 변수 추가: 현재 활성화된 항목
+    let activeItem = null; // 현재 활성화된 항목
 
-    function toggleDetail(item) {
+    // function toggleDetail(item) {
         const detail = item.querySelector('.detail');
         if (activeItem !== item) {
             // 현재 활성화된 항목이 아닌 경우만 visible 클래스 추가
@@ -20,13 +20,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    productItems.forEach(item => {
-        item.addEventListener('click', function() {
-            if (!isScrolling) {
-                toggleDetail(this);
-            }
-        });
+    // 스크롤 이벤트 리스너 추가. 스크롤이 멈출때만 터치가 작동하게 하려고.
+    // 간혹가다가 사진이미지의 내용이 두개가 동시에 뜨는 문제도 해결하려 했으나 완벽하게 해결되진 않음.
+    document.addEventListener('scroll', () => {
+        isScrolling = true;
+        clearTimeout(isScrollingTimeout);
+        var isScrollingTimeout = setTimeout(() => {
+            isScrolling = false;
+        }, 150); // 150ms 이후에 스크롤이 멈춘 것으로 간주
+    });
 
+    productItems.forEach(item => {
         item.addEventListener('touchstart', function(event) {
             if (!isScrolling) {
                 touchStartTime = Date.now();
@@ -45,15 +49,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    document.addEventListener('click', function(event) {
-        if (!event.target.closest('#content .product_list li')) {
-            if (activeItem) {
-                activeItem.querySelector('.detail').classList.remove('visible');
-                activeItem = null;
-            }
-        }
-    });
 
+    // addEventListener의 click은 마우스로 클릭했을때 동작하는 거라서 모바일에선 작동하지 않아서 코드 삭제
+    // document.addEventListener('click', function(event) {
+    //     if (!event.target.closest('#content .product_list li')) {
+    //         if (activeItem) {
+    //             activeItem.querySelector('.detail').classList.remove('visible');
+    //             activeItem = null;
+    //         }
+    //     }
+    // });
+
+    // 모바일에서 터치했을때 동작하게 하려면 click이 아닌 touchstart를 사용해야 한다. 
     document.addEventListener('touchstart', function(event) {
         if (!event.target.closest('#content .product_list li')) {
             if (activeItem) {
@@ -62,19 +69,5 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-
-    window.addEventListener('scroll', function() {
-        clearTimeout(scrollTimeout);
-        isScrolling = true;
-        
-        scrollTimeout = setTimeout(function() {
-            isScrolling = false;
-        }, 200);
-
-        // 스크롤 중에는 활성화된 항목의 visible 클래스 제거
-        if (activeItem) {
-            activeItem.querySelector('.detail').classList.remove('visible');
-            activeItem = null;
-        }
-    });
 });
+    
